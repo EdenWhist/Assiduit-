ca089919b03aebe4b1e3ce9040509bd38818431f
EmmaNDONGOChristi <edenwhist@gmail.com>
2023-01-08 00:10:40 +0100

    J'ai encore avancé avec le TAF je continue demain

--- app/src/main/java/com/example/assiduity/Employe.java
+++ app/src/main/java/com/example/assiduity/Employe.java
@@ -1,2 +1,39 @@
-package com.example.assiduity;public class Employe {
+package com.example.assiduity;
+
+public class Employe {
+    String nom,prenom,email,adresse,mdp;
+    String tel;
+
+    public Employe(String nom, String prenom, String email, String adresse, String mdp, String tel) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.adresse = adresse;
+        this.mdp = mdp;
+        this.tel = tel;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public String getTel() {
+        return tel;
+    }
 }
--- app/src/main/java/com/example/assiduity/Employee.java
+++ app/src/main/java/com/example/assiduity/Employee.java
@@ -1,14 +1,111 @@
 package com.example.assiduity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Employee extends AppCompatActivity {
+    EditText name,surname,address,phone,mail,key;
+    Button add;
+    DatabaseReference employeeDb;
+    FirebaseAuth fauth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee);
+
+        name=findViewById(R.id.name);
+        surname=findViewById(R.id.surname);
+        address=findViewById(R.id.address);
+        phone=findViewById(R.id.phone);
+        mail=findViewById(R.id.mail);
+        key=findViewById(R.id.key);
+        add=findViewById(R.id.create);
+
+        employeeDb= FirebaseDatabase.getInstance().getReference().child("employee");
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addEmployeeData();
+            }
+        });
+
+
+    }
+    private void addEmployeeData(){
+        String nom=name.getText().toString();
+        String prenom=surname.getText().toString();
+        String adresse=address.getText().toString();
+        String email=mail.getText().toString();
+        String mdp=key.getText().toString();
+        String tel= phone.getText().toString();
+
+        if(TextUtils.isEmpty(nom)){
+            name.setError("name is required!");
+            return;
+        }
+        if(TextUtils.isEmpty(prenom)){
+            surname.setError("surname is required!");
+            return;
+        }
+        if(TextUtils.isEmpty(adresse)){
+            address.setError("address is required!");
+            return;
+        }
+        if(TextUtils.isEmpty(tel)){
+            phone.setError("Phone number is required!");
+            return;
+        }
+        if(TextUtils.isEmpty(email)){
+            mail.setError("email is required!");
+            return;
+        }
+        if(TextUtils.isEmpty(mdp)){
+            key.setError("Password is required!");
+            return;
+        }
+        if(mdp.length()<8){
+            key.setError("Password is too short, we need a password of at least 8 characters!");
+            return;
+        }
+
+        if(fauth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),Staff.class));
+            finish();
+        }
+        Employe employe= new Employe(nom,prenom,email,adresse,mdp,tel);
+        employeeDb.push().setValue(employe);
+
+        fauth.createUserWithEmailAndPassword(email,mdp).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(Employee.this, "Employee add!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(),Staff.class));
+                    finish();
+
+                }else{
+                    Toast.makeText(Employee.this, "We have an error!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
